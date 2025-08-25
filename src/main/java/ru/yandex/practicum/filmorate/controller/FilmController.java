ce2db0f9895ef87b1fd70daf6939639ac125e68d
KAPJ1C0H <polyakoff.art3m@ya.ru>
2025-08-25 18:17:40 +0300

    ТЗ10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,30 @@
+
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findAll(){
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create (@RequestBody Film film) {
+
+    }
+
+    @PutMapping
+    public Film update (@RequestBody Film newFilm) {
+
+    }
 }
